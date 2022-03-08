2f7e0ee8e1680aede7c1de4c9b51b84b2c8cdca7
kwabena <kwabena@mpharma.com>
2022-03-08 22:25:57 +0000

    Add django middleware and tracing client

---
+++ sentry_tracing_client.py
@@ -0,0 +1,16 @@
+import sentry_sdk
+import uuid
+
+class SentryTracing:
+    sentry_trace = None
+    
+    def start_trace(self):
+        trace_id = uuid.uuid4().hex
+        self.sentry_trace =  trace_id + "-" + uuid.uuid4().hex[16:]
+
+        transaction = sentry_sdk.Hub.current.scope.transaction
+        if transaction:
+            transaction.trace_id=trace_id
+            return self.sentry_trace
+            
+        return None
